07bd3f6128a570f5753e0340a37b4cf64b2e963c
sourabh <raisourabh85@gmail.com>
2024-06-11 12:35:33 +0530

    Navbar Complete

---
+++ qtify/src/components/Search/Search.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from "./Search.module.css"
+// import SearchIcon from "../../assets/SearchIcon.svg"
+import SearchIcon from '@mui/icons-material/Search';
+
+const Search = () => {
+  return (
+    <div >
+      <form className={styles.wrapper}  >
+        <input className={styles.search} />
+        <div>
+            <button className={styles.searchButton} type='submit' >
+              <SearchIcon/>
+            </button>
+        </div>
+      </form>
+        </div>
+  )
+}
+
+export default Search
