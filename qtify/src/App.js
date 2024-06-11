1270a4fe34cc80cb815f36e0b4facbc0b34e3b7c
sourabh <raisourabh85@gmail.com>
2024-06-11 13:55:08 +0530

    HeroSection Complete

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from "./components/NavBar/NavBar.jsx"
+import HeroSection from "./components/HeroSection/HeroSection.jsx"
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <HeroSection/>
     </div>
   );
 }
